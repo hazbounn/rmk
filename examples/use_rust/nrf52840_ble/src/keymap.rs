6f114c848b8ab64fd99ea77ee1a363f99ffb2a6f
Nicolas Hazboun <hazbounnicolas@gmail.com>
2026-01-18 21:30:25 -0500

    start layer 1

--- examples/use_rust/nrf52840_ble/src/keymap.rs
+++ examples/use_rust/nrf52840_ble/src/keymap.rs
@@ -1,20 +1,20 @@
 use rmk::types::action::{EncoderAction, KeyAction};
-use rmk::{a, encoder, k, layer, mn, mo};
+use rmk::{a, encoder, k, layer, mccd, mcci, mn, mo};
 use wmidi;
 pub(crate) const COL: usize = 4;
 pub(crate) const ROW: usize = 6;
 pub(crate) const NUM_LAYER: usize = 1;
-pub(crate) const NUM_ENCODER: usize = 2;
+pub(crate) const NUM_ENCODER: usize = 8;
 
 #[rustfmt::skip]
 pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
     [
         layer!([
+            [k!(A), k!(S), k!(D), k!(F)],
+            [k!(Z), k!(X), k!(C), k!(V)],
+            [k!(Kc1), k!(Kc2), k!(Kc3), k!(Kc4)],
             [k!(Q), k!(W), k!(E), k!(R)],
-            [k!(Q), k!(W), k!(E), k!(R)],
-            [k!(Q), k!(W), k!(E), k!(R)],
-            [k!(Q), k!(W), k!(E), k!(R)],
-            [k!(Q), k!(W), k!(E), k!(R)],
+            [k!(Z), k!(X), k!(C), k!(V)],
             [mn!(wmidi::Channel::Ch1, wmidi::Note::C4, wmidi::Velocity::MAX), k!(W), k!(E), k!(R)]
         ]),
     ]
@@ -22,7 +22,37 @@ pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
 
 pub const fn get_default_encoder_map() -> [[EncoderAction; NUM_ENCODER]; NUM_LAYER] {
     [[
-        encoder!(k!(KbVolumeUp), k!(KbVolumeDown)),
-        encoder!(k!(KbVolumeUp), k!(KbVolumeDown)),
+        encoder!(
+            mcci!(wmidi::Channel::Ch1, wmidi::ControlFunction::MODULATION_WHEEL, 5),
+            mccd!(wmidi::Channel::Ch1, wmidi::ControlFunction::MODULATION_WHEEL, 5)
+        ),
+        encoder!(
+            mcci!(wmidi::Channel::Ch1, wmidi::ControlFunction::BREATH_CONTROLLER, 5),
+            mccd!(wmidi::Channel::Ch1, wmidi::ControlFunction::BREATH_CONTROLLER, 5)
+        ),
+        encoder!(
+            mcci!(wmidi::Channel::Ch1, wmidi::ControlFunction::UNDEFINED_3, 5),
+            mccd!(wmidi::Channel::Ch1, wmidi::ControlFunction::UNDEFINED_3, 5)
+        ),
+        encoder!(
+            mcci!(wmidi::Channel::Ch1, wmidi::ControlFunction::FOOT_CONTROLLER, 5),
+            mccd!(wmidi::Channel::Ch1, wmidi::ControlFunction::FOOT_CONTROLLER, 5)
+        ),
+        encoder!(
+            mcci!(wmidi::Channel::Ch1, wmidi::ControlFunction::PORTAMENTO_TIME, 5),
+            mccd!(wmidi::Channel::Ch1, wmidi::ControlFunction::PORTAMENTO_TIME, 5)
+        ),
+        encoder!(
+            mcci!(wmidi::Channel::Ch1, wmidi::ControlFunction::DATA_ENTRY_MSB, 5),
+            mccd!(wmidi::Channel::Ch1, wmidi::ControlFunction::DATA_ENTRY_MSB, 5)
+        ),
+        encoder!(
+            mcci!(wmidi::Channel::Ch1, wmidi::ControlFunction::CHANNEL_VOLUME, 5),
+            mccd!(wmidi::Channel::Ch1, wmidi::ControlFunction::CHANNEL_VOLUME, 5)
+        ),
+        encoder!(
+            mcci!(wmidi::Channel::Ch1, wmidi::ControlFunction::BALANCE, 5),
+            mccd!(wmidi::Channel::Ch1, wmidi::ControlFunction::BALANCE, 5)
+        ),
     ]]
 }
